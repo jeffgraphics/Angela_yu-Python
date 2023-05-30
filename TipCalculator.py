567c2ce5f73a265daaed6425d021adb565bec175
Jephtah Paul <jeffgraphics@gmail.com>
2023-05-30 10:57:14 +0100

    ca

---
+++ TipCalculator.py
@@ -0,0 +1,4 @@
+total_bill = int(input("What is the total bill"))
+tip_percentage=int(input("What percentage tip you would like to give? 10, 15, or 20"))
+bill_split=int(input("how many people to split the bill"))
+bill_plus_tip = total_bill
